5f0844e1a492be4c89b2754ad0c14fcf75286566
kizrrum <127617474+kizrrum@users.noreply.github.com>
2026-05-02 23:50:57 +0500

    1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,23 @@ app.use(express.static(__dirname));
 
 app.get('/words.txt', (req, res) => {
     res.sendFile(path.join(__dirname, 'words.txt'));
+});
+
+//
+// 🎵 ДОБАВЛЯЕМ ЭТО
+//
+app.get('/midi-list', (req, res) => {
+    const dir = path.join(__dirname, 'music');
+
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Ошибка чтения папки music');
+        }
+
+        const midi = files.filter(f => f.toLowerCase().endsWith('.mid'));
+        res.json(midi);
+    });
 });
 
 app.listen(PORT, () => {
